c5694a05c21a2032b02b6b2199255abaeaf98032
Pete Birkinshaw <pete@binary-ape.org>
2024-06-26 18:56:27 +0100

    Advanced auth options in expertMode

--- src/components/DestinationSelector/AccessButton.vue
+++ src/components/DestinationSelector/AccessButton.vue
@@ -3,38 +3,46 @@ import EntityLogo from '@/components/icons/EntityLogo.vue'
 
 const props = defineProps(['service'])
 import { inject } from 'vue'
+import * as redirector from '@/utils/redirector.js'
 
-const appConfig = inject('appConfig')
+//const appConfig = inject('appConfig')
 const expertMode = inject('expertMode')
 
 const service = props.service
+
+console.log("BOOOPLE")
+
+const defaultInitiator = redirector.buildInitiatorURL(service);
+const expertInitiators = redirector.buildDefaultInitiatorURLs(service);
+
+console.log(defaultInitiator)
+console.log(expertInitiators)
 
 </script>
 
 <template>
 
-
-
   <template v-if="service.login_url">
 
-    <div v-if="expertMode === true" :href="service.login_url" class="btn-group">
-      <button type="button" class="btn btn-danger">Access >></button>
-      <button type="button" class="btn btn-danger dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown"
-              aria-expanded="false">
-        <span class="visually-hidden">Toggle Dropdown</span>
-      </button>
-      <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href="#">Action</a></li>
-        <li><a class="dropdown-item" href="#">Another action</a></li>
-        <li><a class="dropdown-item" href="#">Something else here</a></li>
-        <li>
-          <hr class="dropdown-divider">
-        </li>
-        <li><a class="dropdown-item" href="#">Separated link</a></li>
-      </ul>
-    </div>
+    <a v-if="expertMode === true" :href="service.login_url">
+      <div class="btn-group">
+        <button type="button" class="btn btn-danger">Access >></button>
+        <button type="button" class="btn btn-danger dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown"
+                aria-expanded="false">
+          <span class="visually-hidden">Toggle Dropdown</span>
+        </button>
+        <ul class="dropdown-menu">
+          <li><a class="dropdown-item" :href="defaultInitiator">Default</a></li>
+          <li>
+            <hr class="dropdown-divider">
+          </li>
+          <li v-for="(url, name) in expertInitiators" :key="name"><a class="dropdown-item" :href="url">{{ name }}</a>
+          </li>
+        </ul>
+      </div>
+    </a>
 
-    <a v-else :href="service.login_url" class="btn btn-primary ms-auto">Access >></a>
+    <a v-else :href="defaultInitiator" class="btn btn-primary ms-auto">Access >></a>
 
   </template>
 
--- src/utils/redirector.js
+++ src/utils/redirector.js
@@ -1,15 +1,43 @@
+function buildReturnURL(service, request, destination, config) {
 
-function buildReturnURL(service, request, destination,  config) {
+  const returnURL = request.return || destination.return_url
+  const returnParam = request.returnIDParam || 'entityID'
+  const url = new URL(returnURL)
 
-  const returnURL = request.return || destination.return_url;
-  const returnParam = request.returnIDParam || 'entityID';
-  const url = new URL(returnURL);
+  url.searchParams.append(returnParam, service.id)
+  return url.toString()
 
-  url.searchParams.append(returnParam, service.id);
-  return url.toString();
+}
+
+function buildInitiatorURL(service, features = {}) {
+
+  const initiatorURL = service.login_url;
+
+  const url = new URL(initiatorURL);
+
+  for (const [key, value] of Object.entries(features)) {
+    url.searchParams.append(key, value);
+  }
+
+  //url.searchParams.append(features);
+  return url.toString()
+
+}
+
+function buildDefaultInitiatorURLs(service) {
+
+  return {
+    'Passive': buildInitiatorURL(service, { isPassive: true }),
+    'Forced': buildInitiatorURL(service, { forceAuthn: true }),
+    'Password': buildInitiatorURL(service, { authnContextClassRef: 'urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport' }),
+    'REFEDS MFA': buildInitiatorURL(service, { authnContextClassRef: 'https://refeds.org/profile/mfa' }),
+    'REFEDS SFA': buildInitiatorURL(service, { authnContextClassRef: 'https://refeds.org/profile/sfa' })
+  }
 
 }
 
 export {
-  buildReturnURL
-};
+  buildReturnURL,
+  buildInitiatorURL,
+  buildDefaultInitiatorURLs
+}
