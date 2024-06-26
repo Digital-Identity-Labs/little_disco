77457b95265aad8f2e21630ff3d8197fdccff7d3
Pete Birkinshaw <pete@binary-ape.org>
2024-06-26 20:57:23 +0100

    Remove debug message from footer

--- src/components/LayoutFooter.vue
+++ src/components/LayoutFooter.vue
@@ -24,7 +24,6 @@ console.log(expertMode);
           </div>
         </div>
         <div class="col-12 col-lg-auto mt-3 mt-lg-0">
-          Bottom: {{ expertMode }}
         </div>
       </div>
     </div>
