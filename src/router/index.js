f5414d2eb6443630a395b5dd98ffed4abee35599
Pete Birkinshaw <pete@binary-ape.org>
2024-07-12 18:22:48 +0100

    Aah, that's where the warning was coming from

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import { inject } from 'vue'
 
-const appConfig = inject('app_config')
-
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   //history: createWebHistory(config.publicPath),
