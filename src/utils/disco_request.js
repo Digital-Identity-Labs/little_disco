ce430acec14f9a4e466c2730c446bb8cb6608a9d
Pete Birkinshaw <pete@binary-ape.org>
2024-07-13 21:35:59 +0100

    Add a check for duplicate params

--- src/utils/disco_request.js
+++ src/utils/disco_request.js
@@ -1,21 +1,25 @@
-
 function parseRequest(queryParams, agentContext) {
 
-  return Object.freeze(new DiscoRequest(queryParams, agentContext));
+  return Object.freeze(new DiscoRequest(queryParams, agentContext))
 
 }
 
 function DiscoRequest(queryParams, agentContext) {
 
-  this.entityID = queryParams.entityID;
-  this.return = queryParams.return;
-  this.policy = queryParams.policy || 'urn:oasis:names:tc:SAML:profiles:SSO:idp-discovery-protocol:single';
-  this.returnIDParam = queryParams.returnIDParam;
-  this.isPassive = queryParams.isPassive;
-  this.homeMode = queryParams.entityID ? 'origin' : 'destination';
+  this.entityID = queryParams.entityID
+  this.return = queryParams.return
+  this.policy = queryParams.policy || 'urn:oasis:names:tc:SAML:profiles:SSO:idp-discovery-protocol:single'
+  this.returnIDParam = queryParams.returnIDParam
+  this.isPassive = queryParams.isPassive
+  this.homeMode = queryParams.entityID ? 'origin' : 'destination'
+
+  if (typeof this.return == 'string' && typeof this.returnIDParam == 'string' && this.return.includes(this.returnIDParam)) {
+    throw new Error('Duplicate return param')
+  }
+
 }
 
 export {
   parseRequest
-};
+}
 
