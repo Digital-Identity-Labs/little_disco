0fb4802f74806e80814d33895c24d728c7471dc4
Pete Birkinshaw <pete@binary-ape.org>
2024-07-20 09:18:23 +0100

    Services menu now has 3 modes

--- src/components/DestinationSelector.vue
+++ src/components/DestinationSelector.vue
@@ -3,23 +3,30 @@ import DestinationCard from '@/components/DestinationSelector/DestinationCard.vu
 import { inject } from 'vue'
 import * as menuStrategy from '../utils/menu.js'
 
+const appConfig = inject('appConfig')
+const discoDestination = inject('discoDestination')
 
-
-const appConfig = inject('appConfig');
-const discoDestination = inject('discoDestination');
-
-discoDestination.value = null;
+discoDestination.value = null
 
 const destinations = menuStrategy.is(appConfig.menu_provider_type)
 
-const servicesData = destinations.listServices(appConfig);
+const servicesData = destinations.listServices(appConfig)
+
+if (appConfig.index_mode === 'error') {
+  throw new Error('Unspecified destination, no entityID parameter has been set')
+} else if (appConfig.index_mode === 'redirect' && appConfig.index_redirection_url) {
+  // Change title here too, to "redirecting"?
+  console.log(`Redirecting from index to ${appConfig.index_redirection_url}...`)
+  window.location.href = appConfig.index_redirection_url
+}
 
 </script>
 
 <template>
 
-  <div class="container-xl">
-    <div class="row row-cards">
+  <template v-if="appConfig.index_mode === 'menu' && appConfig.simple_menu === true">
+    <div class="container-xl">
+      <div class="row row-cards">
 
         <DestinationCard
           v-for="(service) in servicesData"
@@ -29,7 +36,8 @@ const servicesData = destinations.listServices(appConfig);
           :big="false"
         />
 
+      </div>
     </div>
-  </div>
+  </template>
 
 </template>
